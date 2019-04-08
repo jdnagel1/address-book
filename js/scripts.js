0200435a3b8d7e0bb43bbe043b5cbde76935fad6
Brian Hensley <brnhensley@gmail.com>
2019-04-07 20:21:43 -0700

    added js to add, delete and search contacts

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,40 @@
 // Business Logic for AddressBook
 function AddressBook() {
   this.contacts = []
+  this.currentId = 0
 }
 
+AddressBook.prototype.addContact = function(contact) {
+  contact.id = this.assignId();
+  this.contacts.push(contact);
+}
+
+AddressBook.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+AddressBook.prototype.findContact = function(id) {
+  for (var i=0; i < this.contacts.length; i++) {
+    if (this.contacts[i]) {
+      if (this.contacts[i].id == id) {
+        return this.contacts[i];
+      }
+    }
+  };
+  return false;
+}
+
+AddressBook.prototype.deleteContact = function(id) {
+  for (var i=0; i< this.contacts.length; i++) {
+    if (this.contacts[i]) {
+      if (this.contacts[i].id == id) {
+        return true;
+      }
+    }
+  };
+  return false;
+}
 
 // Business logic for Contacts
 function Contact(firstName, lastName, phoneNumber) {
