1447aa7f0df609bb1d4813937bf681242d1df12a
Brian Hensley <brnhensley@gmail.com>
2019-04-07 19:39:01 -0700

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// Business Logic for AddressBook
+function AddressBook() {
+  this.contacts = []
+}
+
+
+// Business logic for Contacts
+function Contact(firstName, lastName, phoneNumber) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.phoneNumber = phoneNumber;
+}
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
