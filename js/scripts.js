56f22b1d0a2c5d6c1e257caae28c260277d19c70
Brian Hensley <brnhensley@gmail.com>
2019-04-08 20:00:29 -0700

    add method to collect contacts and list them by their ID on the html

--- js/scripts.js
+++ js/scripts.js
@@ -50,6 +50,15 @@ Contact.prototype.fullName = function() {
 //  User Interface Logic -------------
 var addressBook = new AddressBook();
 
+function displayContactDetails(addressBookToDisplay) {
+  var contactsList = $("ul#contacts");
+  var htmlForContactInfo = "";
+  addressBookToDisplay.contacts.forEach(function(contact) {
+    htmlForContactInfo += "<li id=" + contact.id + ">" + contact.firstName + " " + contact.lastName + "</li>";
+  });
+  contactsList.html(htmlForContactInfo);
+};
+
 $(document).ready(function() {
   $("form#new-contact").submit(function(event) {
     event.preventDefault();
@@ -58,6 +67,6 @@ $(document).ready(function() {
     var inputtedPhoneNumber = $("input#new-phone-number").val();
     var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
     addressBook.addContact(newContact);
-    console.log(addressBook.contacts);
+    displayContactDetails(addressBook);
   })
 })
